aef2a0ca4af642933329ff3b65080d76dac1eea5
Jaya prakash L <123235911+jayaprakashll@users.noreply.github.com>
2025-03-20 09:03:00 +0530

    Update tf_to_pkl.py

--- models/tf_to_pkl.py
+++ models/tf_to_pkl.py
@@ -1,1 +1,15 @@
+import tensorflow as tf
+import pickle
+
+keras_model = tf.keras.models.load_model("model.keras")
+
+model_data = {
+    "architecture": keras_model.to_json(), 
+    "weights": keras_model.get_weights()   
+}
+
+with open("model.pkl", "wb") as f:
+    pickle.dump(model_data, f)
+
+print("Model successfully converted to model.pkl")
 
